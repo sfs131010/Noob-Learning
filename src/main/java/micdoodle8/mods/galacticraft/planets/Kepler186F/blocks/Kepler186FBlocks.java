e016de6b5d0210e5dde34335e44a5e617e56498f
sfs131010 <koster235@gmail.com>
2017-03-30 09:34:47 +0800

    Create Kepler186FBlocks.java

---
+++ src/main/java/micdoodle8/mods/galacticraft/planets/Kepler186F/blocks/Kepler186FBlocks.java
@@ -0,0 +1,16 @@
+package micdoodle8.mods.galacticraft.planets.Kepler186F.blocks;
+
+import micdoodle8.mods.galacticraft.core.GCBlocks;
+import micdoodle8.mods.galacticraft.core.items.ItemBlockDesc;
+import micdoodle8.mods.galacticraft.core.items.ItemBlockGC;
+import micdoodle8.mods.galacticraft.core.util.GCCoreUtil;
+import micdoodle8.mods.galacticraft.planets.Kepler186.items.ItemBlockAsteroids;
+import micdoodle8.mods.galacticraft.planets.Kepler186.items.ItemBlockShortRangeTelepad;
+import micdoodle8.mods.galacticraft.planets.Kepler186.items.ItemBlockWalkway;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.oredict.OreDictionary;
